6ae41d24495673a9877e8650290299139bee41ea
Wilder Rincon <1588@holbertonschool.com>
2020-02-06 14:52:40 -0500

    Adding changes 6. file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,11 @@
 */
 int main(void)
 {
-printf("Size char: %\n", sizeof(char));
-printf("Size int: %i\n", sizeof(int));
-printf("Size long int: %i\n", sizeof(long int));
-printf("Size float: %i\n", sizeof(float));
-printf("Size double: %i\n", sizeof(double));
+char 
+printf("Size of a char: %i\n", sizeof(char));
+printf("Size of a int: %i\n", sizeof(int));
+printf("Size of a long int: %i\n", sizeof(long int));
+printf("Size of a float: %i\n", sizeof(float));
+printf("Size of a  double: %i\n", sizeof(double));
 return (0);
 }
