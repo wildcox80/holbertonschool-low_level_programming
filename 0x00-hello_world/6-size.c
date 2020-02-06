7465e96fc31793d7efff666d678425dd5de6ed81
Wilder Rincon <1588@holbertonschool.com>
2020-02-06 07:05:23 +0000

    Adding changes 6. file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-char varChar;
-int varInt;
-long int varLint;
-float varFloat;
-double varDouble
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an  int: %ld byte(s)\n", sizeof(int));
-printf("Size of a  long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a  float: %ld byte(s)\n", sizeof(float));
-printf("Size of a  double: %ld byte(s)\n", sizeof(double));
+char charVar;
+int intVar;
+long int lintVar;
+long long int llintVar;
+float floatVar;
+printf("Size of a char: %ld byte(s)\n", sizeof(charVar));
+printf("Size of an int: %ld byte(s)\n", sizeof(intVar));
+printf("Size of a long int: %ld byte(s)\n", sizeof(lintVar));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(llintVar));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatVar));
 return (0);
 }
