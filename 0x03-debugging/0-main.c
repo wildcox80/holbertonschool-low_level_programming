a8af8a29798f4c1e197fae014ef7e91870d27915
Wilder Rincon <1588@holbertonschool.com>
2020-02-11 16:55:20 -0500

    Adding changes 0. file Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "holberton.h"
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = 0;
-printf("%i is zero\n", n);
-return (0);
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
