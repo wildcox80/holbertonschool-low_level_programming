94f0475602104c23631ac2a75a50ee7b589e7c25
Wilder Rincon <1588@holbertonschool.com>
2020-02-11 10:04:20 -0500

    Adding changes 7. file last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_last_digit - selects the last digit of an integer
+ * @n: int type
+ *
+ * Return: the absolute value of last digit
+ */
+int print_last_digit(int n)
+{
+int ld;
+if (n < 0)
+{
+	ld = -1 * (n % 10);
+	_putchar(n + '0');
+	return (ld);
+}
+else
+{
+	ld = n % 10;
+	_putchar(ld + '0');
+	return (ld);
+}
+}
