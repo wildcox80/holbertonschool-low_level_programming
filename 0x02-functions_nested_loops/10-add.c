a54095cee057dbfa7d3793c8563779d8a1a00765
Wilder Rincon <1588@holbertonschool.com>
2020-02-11 14:43:09 -0500

    Adding changes 10. file add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - adds two integers and returns the result.
+* @f: variable 1
+* @g: variable 2
+* Return: add var1 + var2.
+*/
+int add(int f, int g)
+{
+	return (f + g);
+}
