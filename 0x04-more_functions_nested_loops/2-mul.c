5aea1b8563836fa7f50e51a73e6c9d38c10ad0e9
Wilder Rincon <1588@holbertonschool.com>
2020-02-13 11:23:24 -0500

    Adding changes 2. file multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _ismul - multiplies two integers.
+ * @a : int type number.
+ * @b : int type number.
+ * Return: 100352 -1646592.
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+return (0);
+}
