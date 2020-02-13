ee082f3a3c251577c2da908ad40de8193e952df2
Wilder Rincon <1588@holbertonschool.com>
2020-02-13 10:31:32 -0500

    Adding folder 0x04 more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c : int type uppercase.
+ * Return: A is : 1 or a is : 0.
+ */
+int _isupper(int c)
+{
+if
+(c > 64 && c < 91)
+return (1);
+
+return (0);
+}
