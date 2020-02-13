458b2fe4365a01b3c546eb94a128a3dd80cf94e8
Wilder Rincon <1588@holbertonschool.com>
2020-02-13 15:49:01 -0500

    Adding changes 5. file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * most_numbers - Print numbers 0 to 9, minus 2 and 4.
+ * Description: use only _putchar
+ */
+void most_numbers(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < 15)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			_putchar(b + '0');
+			b++;
+		}
+
+		a++;
+	}
+	_putchar('\n');
+}
