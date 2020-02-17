72129d0947af73f47dd65e98e60d3e5d0a79c1f9
Wilder Rincon <1588@holbertonschool.com>
2020-02-17 16:59:40 -0500

    Adding folder and files 0x05 project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	
+}
