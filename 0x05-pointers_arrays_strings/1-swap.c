8e8dbaee9f925f1e8ee974956b55ec5ec0404f4c
Wilder Rincon <1588@holbertonschool.com>
2020-02-18 13:30:51 -0500

    Adding changes file 1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
 #include "holberton.h"
 
 /**
- *reset_to_98 - updates a integer value to 98
- *@n: A pointer to an int that will be updated
+ *swap_int - updates a integer value to 98
+ *@a: Variable a
+ *@b: Variable b
  *
  *Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
