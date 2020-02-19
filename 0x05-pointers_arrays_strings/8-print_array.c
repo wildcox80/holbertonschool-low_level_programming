2964bb2c6fd76e97eddb631832e011e20db321d4
Wilder Rincon <1588@holbertonschool.com>
2020-02-19 15:39:55 -0500

    Adding changes file 8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints half of a string.
+ * @a: int type array pointer
+ * @n: int type integer
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+	if (i < (n - 1))
+{
+	printf(",");
+}
+}
+
+printf("\n");
+}
