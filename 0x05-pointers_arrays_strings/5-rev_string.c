3bb7e0a3f48bb88d7ff2b576c63d9ea8c01c89fc
Wilder Rincon <1588@holbertonschool.com>
2020-02-19 21:28:15 -0500

    Adding changes file 5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,46 @@
 #include "holberton.h"
+/**
+ * _strlen - returns the length of the string -1
+ * @s: the string to get the length
+ *
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		++count;
+		++a;
+	}
+	--count;
+	return (count);
+}
 
 /**
- * rev_string - prints a string, in reverse, followed by a new line.
- * @s: type char.
- * Return: Always 0.
+ * rev_string - prints a string in reverse order
+ * @s: the string to count down from
+ *
+ * Description: Reverses the string in memory
+ * Return: void
  */
 void rev_string(char *s)
 {
-int a;
-for (a = 0; s[a] != '\0'; a++)
-{
-_putchar(s[a]);
+
+	int b, c;
+	int length = _strlen(s);
+	char h;
+
+	b = 0;
+	c = length;
+	while (c >= b)
+	{
+		h = s[b];
+		s[b] = s[c];
+		s[c] = h;
+		c--;
+		b++;
+	}
 }
