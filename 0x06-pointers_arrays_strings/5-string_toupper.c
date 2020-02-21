f17cc7bc4063e44ad1d0a2ccd44178a67c679475
Wilder Rincon <1588@holbertonschool.com>
2020-02-20 23:03:10 -0500

    Adding changes file 5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
 
 char *string_toupper(char *s)
 {
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
+		s[i] = s[i] - 32;
 	}
-	return (s);
+}
+return (s);
 }
