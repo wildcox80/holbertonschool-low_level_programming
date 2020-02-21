828745be756bc6e7b6330f376aba477ab38c42ed
Wilder Rincon <1588@holbertonschool.com>
2020-02-20 23:00:44 -0500

    Adding changes file 5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - change a string to upper case
+ * @s: pointer to a string
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
