3ebeaeeaa9411393c414098e778e627ec301f5db
Wilder Rincon <1588@holbertonschool.com>
2020-02-26 15:07:14 -0500

    Adding changes file 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,15 +14,5 @@ void _puts_recursion(char *s)
 	}
 }
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Betty Holberton");
-    return (0);
-}
 
 	
