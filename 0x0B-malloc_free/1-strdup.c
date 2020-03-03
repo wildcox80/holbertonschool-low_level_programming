9f009e74c6b0f6541e7380bb85084f36c42f34f3
Wilder Rincon <1588@holbertonschool.com>
2020-03-03 14:37:19 -0500

    Adding file 1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: pointer string
+ * Return: Return pointer to array created
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+	int counter = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	s = malloc(counter);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < counter + 1)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	return (s);
+}
