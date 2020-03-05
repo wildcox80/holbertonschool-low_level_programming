7ef2990b4b9964cc6f6a9222d1b42356474c508f
Wilder Rincon <1588@holbertonschool.com>
2020-03-04 23:01:00 -0500

    Adding changes file 100-strtow

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,31 +1,73 @@
 #include "holberton.h"
 #include <stdlib.h>
+/**
+ * wrdcnt - counts the number of words in a string
+ * @s: string to count
+ * Return: int of number of words
+ **/
+int wrdcnt(char *s)
+{
+	int i, n = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] != ' ' && s[i + 1] != '\0')
+				n++;
+		}
+		else if (i == 0)
+			n++;
+	}
+	n++;
+	return (n);
+}
 
 /**
- * strtow - slpits a string into words.
- * @str: string
- * Return: Null if fail, else return pointer to new string
- */
-
+ * strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings
+ **/
 char **strtow(char *str)
 {
-	int i, x;
-	char *a;
+	int i, j, k, l, n = 0, wc = 0;
+	char **w;
 
-	if (str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
+	n = wrdcnt(str);
+	if (n == 1)
+		return (NULL);
+	w = (char **) malloc(n * sizeof(char *));
+	if (w == NULL)
+		return (NULL);
+	w [n - 1] = NULL;
 	i = 0;
-	while (str[i] != 0)
-		i++;
-
-	a = malloc(i * sizeof(char) + 1);
-
-	if (a == 0)
-		return (NULL);
-
-	for (x = 0; x < i; x++)
-		a[x] = str[x];
-
-	return (a);
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			w[wc] = (char *) malloc(j * sizeof(char));
+			j--;
+			if (w[wc] == NULL)
+			{
+				for (k = 0; k < wc; k++)
+					free (w[k]);
+				free (w[n - 1]);
+				free (w);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				w [wc][l] = str[i + l];
+			w [wc][l] = '\0';
+			wc++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (w);
 }
