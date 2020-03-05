b2f5dc318182631404a5c33e992cedba839698c9
Wilder Rincon <1588@holbertonschool.com>
2020-03-04 22:42:58 -0500

    Adding changes file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-<<<<<<< HEAD
-char 
-printf("Size of a char: %i\n", sizeof(char));
-printf("Size of a int: %i\n", sizeof(int));
-printf("Size of a long int: %i\n", sizeof(long int));
-printf("Size of a float: %i\n", sizeof(float));
-printf("Size of a  double: %i\n", sizeof(double));
-=======
 char charVar;
 int intVar;
 long int lintVar;
@@ -24,6 +16,5 @@ printf("Size of an int: %ld byte(s)\n", sizeof(intVar));
 printf("Size of a long int: %ld byte(s)\n", sizeof(lintVar));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(llintVar));
 printf("Size of a float: %ld byte(s)\n", sizeof(floatVar));
->>>>>>> f3435780b90534628eee3e0eb5f3d14d04e0cc81
 return (0);
 }
