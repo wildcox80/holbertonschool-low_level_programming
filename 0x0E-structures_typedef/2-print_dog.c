ef9239630f796373bb5267ef6e6bcb89f5818708
Wilder Rincon <1588@holbertonschool.com>
2020-03-09 23:03:20 -0500

    Adding changes file 2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,25 +8,16 @@
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-
-	return;
-
-if (d->name == NULL)
-
-	printf("name: (nil)\n");
-
-else
-
-	printf("name: %s\n", d->name);
-printf("age: %f\n", d->age);
-
-if (d->owner == NULL)
-
-	printf("Owner: (nil)\n");
-
-else
-
-	printf("Owner: %s\n", d->owner);
-
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
+
