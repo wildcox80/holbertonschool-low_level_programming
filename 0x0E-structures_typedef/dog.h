677a89140761c0d40056b561a9ba3a988612e318
Wilder Rincon <1588@holbertonschool.com>
2020-03-09 22:19:50 -0500

    Adding file dog.h 0-Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog info
+ * @name: type char
+ * @age: float type
+ * @owner: char type
+ *
+ *Description: structure to dog info
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
