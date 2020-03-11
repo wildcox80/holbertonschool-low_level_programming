0039698d63762930a5de1208a14564c275d08a8d
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-11 15:11:03 -0500

    Adding changes file 2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: pointer to array of int type
+ * @size: type of size_t typedef
+ * @cmp: pointer to function
+ * Return: always successful
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
