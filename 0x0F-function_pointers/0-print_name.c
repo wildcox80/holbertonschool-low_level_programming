2a6566f97f42873c084387e2cdcdf92dd49e2192
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-11 13:31:50 -0500

    Adding header and file 0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: pointer of char
+ * @f: pointer to function
+ * Return: Always successful
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return (0);
+	f(name);
+}
