c3b8fce7fa58c6d666bee8c8f2e99b1e1adeb870
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-11 23:17:15 -0500

    Adding changes file 3-main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,41 @@
+#include <stdio.h>
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * main - calculator
- * @argc: number of args
- * @argv: args
- * Return: 0. 98. 99. 100
+ * main - Entry point of program
+ * @argc: size of array argv and number of command line arguments
+ * @argv: An array of size argc
+ * Return: Always successful
  */
 
 int main(int argc, char *argv[])
 {
-	char *str, oper;
-	int num1, num2, tot;
+	int (*operator)(int, int);
+	int num1, num2;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	oper = argv[2][0];
-	str = argv[2];
+	operator = get_op_func(argv[2]);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(str) == NULL || str[1] != '\0')
+	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((oper == '/' || oper == '%') && num2 == 0)
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	tot = (*get_op_func(str))(num1, num2);
-	printf("%d\n", tot);
+
+	printf("%d\n", operator(num1, num2));
+
 	return (0);
 }
-
