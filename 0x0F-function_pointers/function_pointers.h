c6c3763cbc97e81e5102b3b7aa8d774a2b418021
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-11 23:00:09 -0500

    Adding changes file function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
+#ifndef function_pointers_h
+#define function_pointers_h
 #include <stdlib.h>
 
 int _putchar(char c);
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-
