8ed17e718db8687cea2ce6d8e145373dead7ee38
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-16 00:09:58 -0500

    Adding changes header variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,34 +4,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int _putchar(char c);
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+
+void print_string(va_list);
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+
+
 /**
-  * struct pter - pointer to print all
-  * @test: pointer to input for test
-  * @printer: pointer to function
-  */
-typedef struct pt
+  * struct opt - Typedef struct
+  *
+  * @op: Format
+  * @f: The function associated
+  **/
+typedef struct opt
 {
-char *pt;
-void (*printer)();
-} pt_t;
-
-void print_int(va_list arg);
-
-void print_char(va_list arg);
-
-void print_float(va_list arg);
-
-void print_string(va_list arg);
-
-void print_all(const char * const format, ...);
-
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
+	char *op;
+	void (*f)(va_list);
+} p_opt;
 
 #endif
