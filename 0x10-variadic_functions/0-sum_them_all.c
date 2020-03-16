fd707d3e16205de8c958e6c7c41326d7571b7976
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-15 23:16:52 -0500

    Adding changes file 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,28 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+
 /**
- * sum_them_all - sum of all its parameters.
- *@n: num
- * Return: Sum.
+ * sum_them_all - variadic function to add number
+ * @n: const unsigned int type
+ * Return: sum of numbers
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list sum_num;
-	double sum = 0;
+	unsigned int sum = 0;
 	unsigned int i;
+	va_list addlist;
 
-	va_start(sum_num, n);
-
+	va_start(addlist, n);
 	if (n == 0)
 	{
 		return (0);
 	}
-	i = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(addlist, unsigned int);
 	}
-	va_end(sum_num);
-
+	va_end(addlist);
 	return (sum);
 }
+
