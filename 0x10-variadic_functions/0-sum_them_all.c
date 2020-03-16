124271044c7a122babe27e03b06ed2255dc46a21
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-15 22:57:06 -0500

    Adding changes file 0-sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
-	unsigned int i;
 	va_list addlist;
+	double sum = 0;
+	unsigned int i;
 
 	va_start(addlist, n);
 	if (n == 0)
+	{
 		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(addlist, unsigned int);
+		sum += va_arg(addlist, int);
 	}
 	va_end(addlist);
 	return (sum);
