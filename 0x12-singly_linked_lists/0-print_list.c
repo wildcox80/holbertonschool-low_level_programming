3847a2bcd31b69c9bdb869b210969395fb921ce7
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-19 19:57:12 -0500

    Adding changes file 0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+include "lists.h"
+/**
+ * print_list - function to print node contents
+ * @h: pointer to node struct
+ * Return: Always Successful
+ */
+size_t print_list(const list_t *h)
+{
+	int x;
+
+	x = 0;
+	while (h != NULL)
+	{
+		if (h->str ==  NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (x);
+}
