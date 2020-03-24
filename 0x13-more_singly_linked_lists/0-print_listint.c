786f6addce8bd96a4555d56fd007b3fe726676f1
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-23 21:29:26 -0500

    Adding file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - function to print all elements
+ * @h: listint_t type of node struct
+ * Return: Always successful
+ */
+
+size_t print_listint(const listint_t *h)
+
+{
+	int x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
+
