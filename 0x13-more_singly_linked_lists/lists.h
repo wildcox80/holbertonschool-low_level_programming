c94e9dc7f58a3026583b5a2be92b66f7732e9c85
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-24 23:06:09 -0500

    Adding changes file lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,8 +18,8 @@
 
 typedef struct listint_s
 {
-	    int n;
-		struct listint_s *next;
+    int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar (char c);
@@ -32,7 +32,7 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
+int sum_listint(listint_t *head);
 
 #endif  /* LISTS_H */
 
