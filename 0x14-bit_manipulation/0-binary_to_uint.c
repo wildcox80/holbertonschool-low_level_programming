3e5ce08bc16aa15f12ecd9994b118d3e760a26c0
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-03-26 19:05:47 -0500

    Adding changes file 0-binary_to_unit.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - function to convert binary to unsigned int
+ * @b: pointer to char string input
+ * Return: always successful
+ */
+
+/* Function Prototype */
+unsigned int binary_to_uint(const char *b)
+
+{
+	/* Variables */
+	unsigned int x = 0;
+	unsigned int y = 1;
+
+	/* condition is b == NULL */
+	if (b == NULL)
+		return (0);
+
+	/* Loop return converted number */
+	for (int z = 0; b[z] != '\0'; z++)
+	{
+		if (b[z] != '0' && b[z] != '1')
+		{
+			return (0);
+		}
+		x <<= 1;
+
+		if (b[z] == '1')
+		{
+			x ^= y;
+		}
+	}
+	return (x);
+
+}
