2f44b20cc2d72910a062922bdc81f0400bfcf45c
Wilder Rincon <1588@holbertonschool.com>
2020-05-03 20:29:50 +0000

    Delete file 1-main.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints a dlistint_t list
+ * @h: head of dlistint_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
