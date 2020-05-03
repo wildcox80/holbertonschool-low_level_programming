a9d6d617162dd7fa8d7cd0450f9deff276e1f1d5
Wilder Rincon <1588@holbertonschool.com>
2020-05-03 21:45:43 +0000

    Adding file 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	else if (temp != NULL)
 	{
-		new = malloc(sizeof(dlistint_t));
+		new_node = malloc(sizeof(dlistint_t));
 
 		if (new_node == NULL)
 			return (NULL);
