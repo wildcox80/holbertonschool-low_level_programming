4d7e09100f68cec73c476b05a91c1329de9c9e8a
Wilds Rincon <1588@holbertonschool.com>
2020-05-29 15:41:34 -0500

    Adding changes file 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(tmp->value);
 			free(new_node);
 			tmp->value = strdup(value);
+			return (1);
 		}
 		tmp = tmp->value;
 
