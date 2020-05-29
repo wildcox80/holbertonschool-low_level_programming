9c207f01266b7afa4fc65682bea5472e342e9ae2
Wilds Rincon <1588@holbertonschool.com>
2020-05-29 15:40:33 -0500

    Adding changes file 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,47 +4,36 @@
  * @ht: is the hash table to be updated
  * @key: key cannot be empty
  * @value: value to be associated to the key
- * Return: 1 if success, 0 if fail
+ * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i = 0;
-	hash_node_t *new_node, *ptr, *tmp;
-	/* Return NULL if nothing else */
+	hash_node_t *new_node, *tmp;
+
 	if (!ht || !ht->array || !key || !value || strlen(key) == 0)
 		return (0);
 	if (strcmp("", key) == 0)
 		return (0);
-	/* create new node */
+	i = key_index((unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	tmp = ht->array[i];
+	while (tmp != NULL)
 	{
-		free(new_node);
-		return (0);
-	}
-	/* Get hash_key and index */
-	i = key_index((unsigned char *)key, ht->size);
-	if (ht->array[i] != NULL)
-	{
-		tmp = ht->array[i];
-		ptr = tmp;
-		while (tmp)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			if (strcmp(tmp->key, key) == 0)
-			{
-				free(tmp);
-				break;
-			}
-			tmp = tmp->next;
+			free(tmp->value);
+			free(new_node);
+			tmp->value = strdup(value);
 		}
-		/* Add new_node to start linked list */
-		new_node->next = ptr->next;
-		ptr->next = new_node;
+		tmp = tmp->value;
+
 	}
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[i];
 	ht->array[i] = new_node;
 	return (1);
 }
