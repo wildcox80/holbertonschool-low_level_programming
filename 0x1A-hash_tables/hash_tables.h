bea2a3c5188442f9e4475761674dc1396336f299
Wilds Rincon <1588@holbertonschool.com>
2020-05-29 17:14:55 -0500

    Adding changes file hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *create_hash_node(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 
 #endif /* HASH_TABLES */
