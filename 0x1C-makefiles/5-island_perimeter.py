62797ec40bc5362128f3184296d2d39ddb8eb960
Wilder Rincon <1588@holbertonschool.com>
2020-07-03 20:25:16 -0500

    Adding file 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """Python function to return the perimeter of an island"""
+
+    z = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x + 1 == len(grid) or grid[x + 1][y] == 0:
+                    z += 1
+                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
+                    z += 1
+                if x - 1 == -1 or grid[x - 1][y] == 0:
+                    z += 1
+                if y - 1 == -1 or grid[x][y - 1] == 0:
+                    z += 1
+    return(z)
