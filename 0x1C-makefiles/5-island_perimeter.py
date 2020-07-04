09b7deb288cc89d038ba9251ae63d26633cb5e66
Wilder Rincon <1588@holbertonschool.com>
2020-07-03 20:48:44 -0500

    Adding file 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,12 +9,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                elif i == 0 or grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     total += 1
-                elif j == 0 or grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     total += 1
-                elif i == len(grid) - 1 or grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     total += 1
-                elif j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     total += 1
     return total
